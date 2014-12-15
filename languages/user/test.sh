bae11860d8514c4f8e6d064800cd9d332af537d4
brownman <brownman556@gmail.com>
2014-12-15 15:23:50 +0200

    Update test.sh

--- languages/user/test.sh
+++ languages/user/test.sh
@@ -2,7 +2,7 @@ env | grep 'HOME=/home/gitlab_ci_runner'
 env | grep 'PWD=/home/gitlab_ci_runner'
 sudo echo hi
 sudo cat /etc/sudoers
-cat /etc/sudoers
+#cat /etc/sudoers
 
 #pwd
 #test $(pwd) = '/home/gitlab_ci_runner2'
