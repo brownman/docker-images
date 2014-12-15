523dca482161182762fd528751d6552a3d2f6b69
brownman <brownman556@gmail.com>
2014-12-15 14:54:21 +0200

    user: +test sudo command

--- languages/user/test.sh
+++ languages/user/test.sh
@@ -1,5 +1,8 @@
 env | grep 'HOME=/home/gitlab_ci_runner'
 env | grep 'PWD=/home/gitlab_ci_runner'
+sudo echo hi
+sudo cat /etc/sudoers
+cat /etc/sudoers
 
 #pwd
 #test $(pwd) = '/home/gitlab_ci_runner2'
