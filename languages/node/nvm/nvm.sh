b75ef32ab3e2fbe2a55bd0e24f6daf2ebfd13271
brownman <brownman556@gmail.com>
2014-12-16 12:57:45 +0200

    nodejs: validate installation of boweer

--- languages/node/nvm/nvm.sh
+++ languages/node/nvm/nvm.sh
@@ -9,4 +9,4 @@ curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm alias default 0.10.33
 
 # Install brunch
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && npm install -g brunch
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && npm install -g bower
