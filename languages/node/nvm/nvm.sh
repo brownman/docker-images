d3d5020af92155f69ade90542787782ad4fcd87f
brownman <brownman556@gmail.com>
2014-12-16 12:53:53 +0200

    Create nvm.sh

---
+++ languages/node/nvm/nvm.sh
@@ -0,0 +1,12 @@
+# Install nvm
+USER=$USER
+HOME=/home/$USER
+NVM_DIR=$HOME/.nvm
+curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
+
+# Install nodejs/npm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm install v0.10.33
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm alias default 0.10.33
+
+# Install brunch
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && npm install -g brunch
