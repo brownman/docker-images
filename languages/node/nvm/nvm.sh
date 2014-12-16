78f9fcf45ecf720df5b604ee83e16c5931232ccb
brownman <brownman556@gmail.com>
2014-12-16 13:30:14 +0200

    use wget for now

--- languages/node/nvm/nvm.sh
+++ languages/node/nvm/nvm.sh
@@ -2,7 +2,8 @@
 #USER=$USER
 #HOME=/home/$USER
 NVM_DIR=$HOME/.nvm
-curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
+#curl https://raw.githubusercontent.com/creationix/nvm/v0.18.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.20.0/install.sh | bash
 
 # Install nodejs/npm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && nvm install v0.10.33
