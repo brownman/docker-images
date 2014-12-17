63cc56b9b4421fc4d7e11598d6b54579ef35a1f8
brownman <brownman556@gmail.com>
2014-12-17 20:19:06 +0200

    try to run only ./bin/setup

--- languages/gitlab/cmd.sh
+++ languages/gitlab/cmd.sh
@@ -1,3 +1,4 @@
 #export dir_runner="$HOME/runner"
 ssh-keyscan -H $GITLAB_SERVER_FQDN >> $HOME/.ssh/known_hosts
-bundle exec $dir_runner/bin/setup_and_run
+bundle exec $dir_runner/bin/setup
+#setup_and_run
