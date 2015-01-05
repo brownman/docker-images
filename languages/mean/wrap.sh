69753fe660ddebe3f732edf48b043f568f0ae724
brownman <brownman556@gmail.com>
2015-01-05 16:33:19 +0200

    Update wrap.sh

--- languages/mean/wrap.sh
+++ languages/mean/wrap.sh
@@ -1,34 +1,12 @@
 #!/usr/bin/env bash
 set -e
+address='0.0.0.0:3000'
 sudo netstat -ntlp
 pidof mongod | xargs sudo kill -9
 sudo netstat -ntlp
-
-#sudo service mongod status
-#sudo service mongod stop
 env
-
-#docker build -t mean .
-#( docker run -t brownman/mean ) || ( docker build -t mean . )
-
-#docker run -p 27017:27017 -d --name db mongo
 docker run -v /data:/data -p 27017:27017 -p 28017:28017 -d --name mongo_deamon brownman/mongo sh -c 'mongod --rest --httpinterface --smallfiles'
+docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -i brownman/mean bash < test.sh &
+while true; do  curl $address 2>/dev/null && break ; sleep 1 ; done
 
-#docker run -p 3000:3000 --link db:db_1 mean
-
-
-#and mount /app and /public as volumes:
-#-v /Users/mdl/workspace/mean-stack/mean/public:/home/mean/public -v /Users/mdl/workspace/mean-stack/mean/app:/home/mean/app
-
-
-#To enable live reload, forward port 35729 
-#docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -t brownman/mean
-#docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -t brownman/mean bash < test.sh
-
-#docker run -i -t brownman/mean bash < test.sh
-#docker run -p 3000:3000 -p 35729:35729  -i -t brownman/mean bash < test.sh
-#echo 'env; source ~/.bashrc; cd myApp; npm link; mean status; node_modules/grunt-cli/bin/grunt test; node_modules/grunt-cli/bin/grunt' >/tmp/1
-docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -i brownman/mean bash < test.sh
-
-#< test.sh
 
