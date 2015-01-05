9ddaefb5880b52b391a5701b9cf78666ca61cd9c
brownman <brownman556@gmail.com>
2015-01-05 10:48:52 +0200

    Update wrap.sh

--- languages/meanjs/wrap.sh
+++ languages/meanjs/wrap.sh
@@ -1,7 +1,12 @@
 
 docker build -t mean .
 docker run -p 27017:27017 -d --name db mongo
-docker run -p 3000:3000 --link db:db_1 mean
+#docker run -p 3000:3000 --link db:db_1 mean
 
-#To enable live reload, forward port 35729 and mount /app and /public as volumes:
-#docker run -p 3000:3000 -p 35729:35729 -v /Users/mdl/workspace/mean-stack/mean/public:/home/mean/public -v /Users/mdl/workspace/mean-stack/mean/app:/home/mean/app --link db:db_1 mean
+
+#and mount /app and /public as volumes:
+#-v /Users/mdl/workspace/mean-stack/mean/public:/home/mean/public -v /Users/mdl/workspace/mean-stack/mean/app:/home/mean/app
+
+
+#To enable live reload, forward port 35729 
+docker run -p 3000:3000 -p 35729:35729  --link db:db_1 mean
