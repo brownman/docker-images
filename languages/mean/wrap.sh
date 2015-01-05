35d1456cd564a79b62be05e324c1f8f38e70c9ce
brownman <brownman556@gmail.com>
2015-01-05 16:03:33 +0200

    Update wrap.sh

--- languages/mean/wrap.sh
+++ languages/mean/wrap.sh
@@ -27,6 +27,8 @@ docker run -v /data:/data -p 27017:27017 -p 28017:28017 -d --name mongo_deamon b
 
 #docker run -i -t brownman/mean bash < test.sh
 #docker run -p 3000:3000 -p 35729:35729  -i -t brownman/mean bash < test.sh
-docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -t brownman/mean bash -c 'env; source ~/.bashrc; cd myApp; npm link; mean status; node_modules/grunt-cli/bin/grunt test; node_modules/grunt-cli/bin/grunt'
+echo 'env; source ~/.bashrc; cd myApp; npm link; mean status; node_modules/grunt-cli/bin/grunt test; node_modules/grunt-cli/bin/grunt' >/tmp/1
+docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -i brownman/mean bash < /tmp/1
+
 #< test.sh
 
