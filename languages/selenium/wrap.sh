97d16e8b443060713b61b3f05da2da781bb3acf0
brownman <brownman556@gmail.com>
2015-02-10 18:50:18 +0200

    Update wrap.sh

--- languages/selenium/wrap.sh
+++ languages/selenium/wrap.sh
@@ -1,6 +1,21 @@
+
 echo which docker image should I pull ?
+
+
+set -e
+address='0.0.0.0:4444'
+sudo netstat -ntlp
+pidof mongod | xargs sudo kill -9
+sudo netstat -ntlp
+env
+#docker run -v /data:/data -p 27017:27017 -p 28017:28017 -d --name mongo_deamon brownman/mongo sh -c 'mongod --rest --httpinterface --smallfiles'
+#docker run -p 3000:3000 -p 35729:35729  --link mongo_deamon:db -i brownman/mean bash < test.sh &
 
 docker pull vvoyer/docker-selenium-firefox-chrome
 docker run --privileged -p 4444:4444 -p 5999:5999 -d vvoyer/docker-selenium-firefox-chrome
 
-curl 0.0.0.0:4444/status
+
+#test if the the running machine is accessible from the host:
+while true; do  curl $address 2>/dev/null && break ; sleep 1 ; done
+
+curl $address/status
