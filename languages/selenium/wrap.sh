6ad24ecfbea9466117433c208e3b62c11dde0deb
brownman <brownman556@gmail.com>
2015-02-10 18:34:08 +0200

    Update wrap.sh

--- languages/selenium/wrap.sh
+++ languages/selenium/wrap.sh
@@ -1,1 +1,6 @@
 echo which docker image should I pull ?
+
+docker pull vvoyer/docker-selenium-firefox-chrome
+docker run --privileged -p 4444:4444 -p 5999:5999 -d vvoyer/docker-selenium-firefox-chrome
+
+curl 0.0.0.0:4444/status
