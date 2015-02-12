5bfd22dac85baa1e32dae504cd48fda15c1449e2
brownman <brownman556@gmail.com>
2015-02-12 16:01:52 +0200

    Update wrap.sh

--- languages/selenium/wrap.sh
+++ languages/selenium/wrap.sh
@@ -2,20 +2,51 @@
 
 echo which docker image should I pull ?
 
+commander(){ 
+    local args=($@);
+    local cmd="${args[@]}";
+    echo 1>&2 cmd: $cmd;
+    eval "$cmd" || { echo err cmd; exit 0; }
+}
+try(){ 
+    local args=($@);
+    local cmd="${args[@]}";
+    echo 1>&2 try: $cmd;
+    eval "$cmd"
+}
+
+
 #PRINT HOST ENV
-address='0.0.0.0:4444'
-ps fax
-sudo netstat -ntlp
-env
-
-
+set_env(){
+host='0.0.0.0'
+port=4444
+address="$host:$port"
 #RUN TESTS
-docker pull vvoyer/docker-selenium-firefox-chrome
-docker run --privileged -p 4444:4444 -p 5999:5999 -d vvoyer/docker-selenium-firefox-chrome # < test.sh &
-docker ps
+container1='vvoyer/docker-selenium-firefox-chrome'
+container2='eperoumalnaik/docker-chrome-selenium'
+}
 
+intro(){
+ps fax
+netstat -ntlp
+env
+}
 
-
+run(){
+  local container="$1"
+  
+  commander docker pull $container1
+  commander docker run --privileged -p $port:$port -p 5999:5999 -d $container1 # < test.sh &
+  docker ps
+  
 #ACCESS FROM HOST
-while true; do  curl $address &>/dev/null && break || { echo waiting for selenium-server; }; sleep 1 ; done
-curl $address/status
+while true; do  try curl $address &>/dev/null && break || { echo waiting for selenium-server; }; sleep 1 ; done
+commander curl $address/status
+}
+
+
+try run $container1
+try run $container2
+
+
+
