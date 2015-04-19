e5070bbaf90e0a464e8f76a53bbc0028d6ba8c65
brownman <brownman556@gmail.com>
2015-04-19 11:02:15 +0300

    Update wrap.sh

--- languages/node/nvm/wrap.sh
+++ languages/node/nvm/wrap.sh
@@ -1,3 +1,3 @@
 #https://docs.docker.com/reference/commandline/cli/
   docker build -t docker_current .
-  docker run -i docker_current bash < test.sh
+  ( docker run -i docker_current bash < test.sh ) || true
